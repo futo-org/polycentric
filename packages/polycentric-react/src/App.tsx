bcfcc1120fea42ef8909768cb5ba68710ee2ee17
Harpo Roeder <roederharpo@protonmail.ch>
2022-10-12 16:22:02 -0500

    start of navigation overhaul

--- packages/polycentric-react/src/App.tsx
+++ packages/polycentric-react/src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet, Link, useNavigate, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { AppBar, Toolbar, ThemeProvider, createTheme } from '@mui/material';
+import { AppBar, Toolbar, ThemeProvider, Tooltip, IconButton, Avatar, Box, createTheme } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 import * as Core from 'polycentric-core';
 import PostModal from './PostModal';
@@ -27,9 +28,41 @@ type AppProps = {
 function App(props: AppProps) {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [initial, setInitial] = useState(true);
+    const [avatar, setAvatar] = useState<string | undefined>(undefined);
 
     const location = useLocation();
     const navigate = useNavigate();
+
+    const handleOpenProfile = () => {
+        navigate('/profile');
+    };
+
+    const handleOpenNotifications = () => {
+        navigate('/notifications');
+    };
+
+    const loadProfileImage = async () => {
+        if (props.state.identity === undefined) {
+            return;
+        }
+
+        const profile = await Core.DB.loadProfile(props.state);
+
+        if (profile.imagePointer === undefined) {
+            return;
+        }
+
+        const loaded = await Core.DB.loadBlob(
+            props.state,
+            profile.imagePointer,
+        );
+
+        if (loaded === undefined) {
+            return;
+        }
+
+        setAvatar(Core.Util.blobToURL(loaded.kind, loaded.blob));
+    };
 
     useEffect(() => {
         if (
@@ -42,25 +75,70 @@ function App(props: AppProps) {
         setInitial(false);
     }, [location, navigate, props.state.identity]);
 
+    useEffect(() => {
+        const handlePut = (key: Uint8Array, value: Uint8Array) => {
+            loadProfileImage();
+        };
+
+        props.state.level.on('put', handlePut);
+
+        loadProfileImage();
+
+        return () => {
+            props.state.level.removeListener('put', handlePut);
+        };
+    }, []);
+
     return (
         <div>
             <ThemeProvider theme={theme}>
                 {props.state.identity !== undefined && (
                     <AppBar position="sticky">
-                        <Toolbar className="app__header">
-                            <Link to="/explore">Explore</Link>
-                            <Link to="/notifications">Notifications</Link>
-                            <Link to="/">Feed</Link>
-                            <Link to="/profile">Profile</Link>
-                            <Link to="/following">Following</Link>
-                            <Link to="/search">Search</Link>
-                            <a
-                                onClick={() => {
-                                    setModalIsOpen(true);
-                                }}
+                        <Toolbar>
+                            <Box
+                                className="app__header"
                             >
-                                Post
-                            </a>
+                                <Link to="/explore">Explore</Link>
+                                <Link to="/">Feed</Link>
+                                <Link to="/following">Following</Link>
+                                <Link to="/search">Search</Link>
+                                <a
+                                    onClick={() => {
+                                        setModalIsOpen(true);
+                                    }}
+                                >
+                                    Post
+                                </a>
+                            </Box>
+                            <Box sx={{ flexGrow: 1 }} />
+                            <Box>
+                                <Tooltip title="Open Notifications">
+                                    <IconButton
+                                        onClick={handleOpenNotifications}
+                                        sx={{
+                                            p: 0,
+                                            marginRight: '15px',
+                                        }}
+                                        size="large"
+                                        color="inherit"
+                                    >
+                                        <NotificationsIcon />
+                                    </IconButton>
+                                </Tooltip> 
+                                <Tooltip title="Open Profile">
+                                    <IconButton
+                                        onClick={handleOpenProfile}
+                                        sx={{ p: 0 }}
+                                        size="large"
+                                        color="inherit"
+                                    >
+                                        <Avatar
+                                            alt="avatar"
+                                            src={avatar}
+                                        />
+                                    </IconButton>
+                                </Tooltip>
+                            </Box>
                         </Toolbar>
                     </AppBar>
                 )}
