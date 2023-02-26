4bfcb850f7b014269e41f9871e9bc4a9b02e9e49
Harpo Roeder <roedeharpo@protonmail.ch>
2023-02-26 18:24:59 +0000

    Add App Component to harbor skeleton

--- packages/harbor-web/src/index.tsx
+++ packages/harbor-web/src/index.tsx
@@ -1,5 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 import * as PolycentricCore from 'polycentric-core';
 
-async function main() {}
+import * as App from './App';
+
+async function main() {
+    const root = ReactDOM.createRoot(
+        document.getElementById('root') as HTMLElement,
+    );
+
+    root.render(
+        <React.StrictMode>
+            <App.App/>
+        </React.StrictMode>,
+    );
+}
 
 main();
