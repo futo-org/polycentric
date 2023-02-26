9f96eddd6341fa74a53210f0017c6c856e0ce925
Harpo Roeder <roedeharpo@protonmail.ch>
2023-02-26 19:29:57 +0000

    skeleton design

--- packages/harbor-web/src/App.tsx
+++ packages/harbor-web/src/App.tsx
@@ -1,5 +1,95 @@
+import * as MUI from '@mui/material';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const avatar = "https://pbs.twimg.com/profile_images/1382846958159663105/ltolfDyQ_400x400.jpg";
+
+const name = "Louis Rossmann";
+
+const description = "Apple and Apple Accesories";
+
 export function App() {
     return (
-        <h1>Hello</h1>
+        <div
+            style={{
+                position: 'absolute',
+                left: '0px',
+                top: '0px',
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
+            }}
+        >
+            <div
+                style={{
+                    marginTop: '20px',
+                    width: '33%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                }}
+            >
+                <MUI.Avatar
+                    src={avatar}
+                    style={{
+                        display: 'block',
+                        width: '100px',
+                        height: '100px',
+                    }}
+                />
+
+                <p>
+                    {name}
+                </p>
+
+                <p>
+                    {description}
+                </p>
+
+                <MUI.Paper
+                    elevation={3}
+                    style={{
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        marginBottom: '20px',
+                        paddingLeft: '10px',
+                    }}
+                >
+                    <YouTubeIcon />
+                    <p
+                        style={{
+                            flex: '1',
+                            textAlign: 'center',
+                        }}
+                    >
+                        YouTube
+                    </p>
+                </MUI.Paper>
+
+                <MUI.Paper
+                    elevation={3}
+                    style={{
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingLeft: '10px',
+                    }}
+                >
+                    <TwitterIcon />
+                    <p
+                        style={{
+                            flex: '1',
+                            textAlign: 'center',
+                        }}
+                    >
+                        Twitter
+                    </p>
+                </MUI.Paper>
+            </div>
+        </div>
     );
 }
