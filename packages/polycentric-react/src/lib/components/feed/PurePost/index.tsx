62322cdc2d4df839c509a7200e25f145e033fe4a
aiddun <aidan@futo.org>
2023-08-02 01:36:47 -0500

    better

--- packages/polycentric-react/src/lib/components/feed/PurePost/index.tsx
+++ packages/polycentric-react/src/lib/components/feed/PurePost/index.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom'
 import { Profile } from '../../../types/profile'
 import { forwardRef, useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const dateToAgoString = (date: Date) => {
   const diff = Date.now() - date.getTime()
@@ -120,28 +120,44 @@ export const CommentButton = ({ onClick, count }: { onClick: () => void; count?:
 export const PurePost = forwardRef(
   (
     {
-      content,
-      author,
-      publishedAt,
-      topic,
+      main,
+      sub,
     }: {
-      content: string
-      author: Profile
-      publishedAt: Date
-      topic: string
+      main: {
+        content: string
+        author: Profile
+        publishedAt: Date
+        topic: string
+      }
+      sub: {
+        content: string
+        author: Profile
+        publishedAt: Date
+        topic: string
+        ContentLink?: string
+      }
     },
     ref,
   ) => {
     const mainRef = useRef<HTMLDivElement>(null)
-    const [showMore, setShowMore] = useState(false)
+    const subContentRef = useRef<HTMLDivElement>(null)
+    const [contentCropped, setContentCropped] = useState(false)
     const [expanded, setExpanded] = useState(false)
-    const [liked, setLiked] = useState(false)
+    const [subcontentCropped, setsubcontentCropped] = useState(false)
+
+    const { content, author, publishedAt, topic } = main
 
     useEffect(() => {
       if (mainRef.current != null && expanded === false) {
-        setShowMore(mainRef.current.clientHeight < mainRef.current.scrollHeight)
+        setContentCropped(mainRef.current.clientHeight < mainRef.current.scrollHeight)
       }
     }, [content, expanded])
+
+    useEffect(() => {
+      if (subContentRef.current != null && subcontentCropped === false) {
+        setsubcontentCropped(subContentRef.current.clientHeight < subContentRef.current.scrollHeight)
+      }
+    }, [sub, subcontentCropped])
 
     return (
       <Link to="#" className="cursor-default">
@@ -149,47 +165,76 @@ export const PurePost = forwardRef(
           <div className="flex relative overflow-clip">
             <div className="mr-3 md:mr-4 flex-shrink-0 flex flex-col overflow-clip">
               <img src={author.avatarURL} className="rounded-full h-16 w-16 md:h-20 md:w-20" />
-              <div className="sticky top-full pt-3 overflow-clip flex flex-col space-y-2">
+              <div className="sticky top-full overflow-clip">
                 <LikeButton
                   onClick={() => {
-                    setLiked(!liked)
+                    return
                   }}
                   count={69}
-                  clicked={liked}
+                  clicked={false}
                 />
-                <CommentButton onClick={() => ({})} count={420} />
               </div>
             </div>
             <div className="flex-grow">
               <div className="flex w-full justify-between">
-                <Link to="#" className="font-bold text-md ">
-                  {author.name}
-                </Link>
+                <div className="font-bold text-md ">{author.name}</div>
                 <div className="pr-3 md:pr-0 font-light text-gray-500">{dateToAgoString(publishedAt)}</div>
               </div>
-              <Link to="#" className=" text-purple-400 leading-3">
-                {topic}
-              </Link>
-              <main
-                className={
-                  'pt-4 leading-normal whitespace-pre-line text-lg text-gray-900 font-normal overflow-clip' +
-                  (expanded ? '' : ' line-clamp-[7]') +
-                  (showMore && !expanded
-                    ? ` line-clamp-[7] relative
-                    after:top-0 after:left-0  after:w-full after:h-full 
-                    after:bg-gradient-to-b after:from-80% after:from-transparent after:to-white
-                    after:absolute 
-                    `
-                    : '')
-                }
-                ref={mainRef}
-              >
-                {content}
-              </main>
+              <div className=" text-purple-400 leading-3">{topic}</div>
+              <div className="flex flex-col space-y-3">
+                {/* Actual post content */}
+                <main
+                  className={
+                    'pt-4 leading-normal whitespace-pre-line text-lg text-gray-900 font-normal overflow-clip' +
+                    (expanded ? '' : ' line-clamp-[7]') +
+                    (contentCropped && !expanded
+                      ? ` line-clamp-[7] relative
+                  after:top-0 after:left-0  after:w-full after:h-full 
+                  after:bg-gradient-to-b after:from-80% after:from-transparent after:to-white
+                  after:absolute `
+                      : '')
+                  }
+                  ref={mainRef}
+                >
+                  {content}
+                </main>
+                {/* sub.post */}
+                {sub && (
+                  <Link
+                    to={sub.ContentLink || '/'}
+                    className="border rounded-2xl w-full p-5 bg-white overflow-clip flex flex-col space-y-3"
+                  >
+                    <div className="flex">
+                      <img src={sub.author.avatarURL} className="rounded-full h-5 w-5 md:h-10 md:w-10" />
+                      <div className="flex flex-col ml-2 w-full">
+                        <div className="flex justify-between w-full">
+                          <div className="font-bold">{sub.author.name}</div>
+                          <div className="pr-3 md:pr-0 font-light text-gray-500 text-sm">
+                            {dateToAgoString(sub.publishedAt)}
+                          </div>
+                        </div>
+                        <div className=" text-purple-400 leading-3 text-sm">{sub.topic}</div>
+                      </div>
+                    </div>
+                    <main
+                      ref={subContentRef}
+                      className={`line-clamp-[4]  ${
+                        subcontentCropped
+                          ? `relative after:top-0 after:left-0  after:w-full after:h-full 
+                        after:bg-gradient-to-b after:from-20% after:from-transparent after:to-white
+                        after:absolute`
+                          : ''
+                      }`}
+                    >
+                      {sub.content}
+                    </main>
+                  </Link>
+                )}
+              </div>
             </div>
           </div>
-          {showMore && !expanded && (
-            <div className="flex w-full justify-center -mt-5">
+          {contentCropped && !expanded && (
+            <div className="flex w-full justify-center mt-4">
               <button onClick={() => setExpanded(true)} className="bg-gray-200 rounded-full font-bold px-10 z-10 py-3">
                 Read more
               </button>
