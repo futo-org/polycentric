b749f494690cc645e3933f9ca49a96bd3a3b9c00
aiddun <aidan@futo.org>
2023-09-12 16:49:32 -0500

    Fix sign out bug

--- packages/polycentric-react/src/lib/hooks/processHandleManagerHooks.ts
+++ packages/polycentric-react/src/lib/hooks/processHandleManagerHooks.ts
@@ -29,6 +29,7 @@ export function useProcessHandleManagerBaseComponentHook(
         activeStore: undefined,
         processHandle: undefined,
       })
+      await metaStore.unsetActiveStore()
       return undefined
     }
 
