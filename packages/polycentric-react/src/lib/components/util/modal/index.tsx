a631b00b295a782f8108c31cf8672103d1277610
aiddun <aidan@futo.org>
2023-09-20 16:37:10 -0500

    Add mobile-friendly fullscreen modal

--- packages/polycentric-react/src/lib/components/util/modal/index.tsx
+++ packages/polycentric-react/src/lib/components/util/modal/index.tsx
@@ -1,5 +1,7 @@
-import { Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import { IonModal } from '@ionic/react'
+import { Fragment } from 'react'
+import { useIsMobile } from '../../../hooks/ionicHooks'
 const XIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
     <path
@@ -15,12 +17,36 @@ export const Modal = ({
   setOpen,
   open,
   title,
+  desktopClassName,
 }: {
   children: React.ReactNode
   setOpen: (open: boolean) => void
   open: boolean
   title?: string
+  desktopClassName?: string
 }): JSX.Element => {
+  const isMobile = useIsMobile()
+
+  if (isMobile) {
+    return (
+      <IonModal isOpen={open}>
+        <div className="flex flex-col h-full">
+          <div className="flex justify-between items-center py-5 px-7">
+            <h1 className="text-2xl font-semibold leading-6 text-gray-900">{title}</h1>
+            <button
+              className="flex items-center rounded-full hover:bg-gray-50 border p-2"
+              aria-label="Close"
+              onClick={() => setOpen(false)}
+            >
+              <XIcon />
+            </button>
+          </div>
+          <div className="flex-grow px-7 flex flex-col justify-center">{children}</div>
+        </div>
+      </IonModal>
+    )
+  }
+
   return (
     <Transition appear show={open} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={() => setOpen(false)}>
@@ -33,7 +59,7 @@ export const Modal = ({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 backdrop-blur-sm" onClick={() => setOpen(false)} />
+          <div className="fixed inset-0 bg-gray-100 bg-opacity-30" onClick={() => setOpen(false)} />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
@@ -47,8 +73,13 @@ export const Modal = ({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <div className="flex justify-between items-center px-3">
+              <Dialog.Panel
+                className={
+                  'transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle border transition-all' +
+                  desktopClassName
+                }
+              >
+                <div className="flex justify-between items-center px-3 pb-4">
                   <Dialog.Title className="text-2xl font-semibold leading-6 text-gray-900">{title}</Dialog.Title>
                   <button
                     className="flex items-center rounded-full hover:bg-gray-50 border p-2"
