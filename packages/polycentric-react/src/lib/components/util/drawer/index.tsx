a76fa8042fb2287b7d7e3c13f5908a55d2263640
aiddun <aidan@futo.org>
2023-09-26 13:06:47 -0500

    Add per-topic swipe navigation and native-like swipe sidebar

--- packages/polycentric-react/src/lib/components/util/drawer/index.tsx
+++ packages/polycentric-react/src/lib/components/util/drawer/index.tsx
@@ -1,63 +1,53 @@
-import { Fragment } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import './style.css'
 
 export const Drawer = ({
   title,
   children,
-  open,
-  setOpen,
-  side,
+  side = 'left',
+  contentId = 'main',
 }: {
-  open: boolean
-  setOpen: (o: boolean) => void
   title?: string
   children?: React.ReactNode
-  side: 'left' | 'right'
+  side?: 'left' | 'right'
+  contentId?: string
 }) => {
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div
-              className={`pointer-events-none fixed inset-y-0 ${
-                side === 'left' ? 'left-0' : 'right-0'
-              } flex max-w-full ${side === 'left' ? 'pr-10' : 'pl-10'}`}
-            >
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom={side === 'right' ? 'translate-x-full' : '-translate-x-full'}
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo={side === 'right' ? 'translate-x-full' : '-translate-x-full'}
-              >
-                <Dialog.Panel className="pointer-events-auto relative w-screen max-w-[80vw] md:max-w-md">
-                  <div className="flex h-full flex-col overflow-y-auto bg-white py-6 shadow-xl">
-                    <div className="px-4 sm:px-6">
-                      <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">{title}</Dialog.Title>
-                    </div>
-                    <div className="relative mt-6 flex-1 px-4 sm:px-6"> {children ?? <div />}</div>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
+    <IonMenu side={side === 'left' ? 'start' : 'end'} type="reveal" contentId={contentId}>
+      <div className="flex h-full flex-col overflow-y-auto bg-white py-6 shadow-xl">
+        <div className="px-4 sm:px-6">
+          <h2 className="text-base font-semibold leading-6 text-gray-900">{title}</h2>
         </div>
-      </Dialog>
-    </Transition.Root>
+        <div className="relative mt-6 flex-1 px-4 sm:px-6">{children}</div>
+      </div>
+    </IonMenu>
   )
 }
+
+export const DrawerTest = () => (
+  <>
+    <Drawer contentId="main-content" title="Menu" />
+    <IonPage id="main-content">
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">Tap the button in the toolbar to open the menu.</IonContent>
+    </IonPage>
+  </>
+)
+// }
+// export const DrawerTest = () => (
+//   <div>
+//     <Drawer />
+//     <IonPage>
+//       <IonMenuToggle autoHide={false}>
+//         <button>Click to open the menu</button>
+//       </IonMenuToggle>
+//     </IonPage>
+//   </div>
+// )
