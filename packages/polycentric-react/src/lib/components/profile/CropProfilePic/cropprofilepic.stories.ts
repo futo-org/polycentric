52cfb25a1c040de00f5dfe419ea26be76010d09e
Aidan <33-aidan@users.noreply.gitlab.futo.org>
2023-10-04 00:24:47 +0000

    Add cropper for onboarding

---
+++ packages/polycentric-react/src/lib/components/profile/CropProfilePic/cropprofilepic.stories.ts
@@ -0,0 +1,12 @@
+import { CropProfilePic } from '.'
+
+export default {
+  title: 'Components/CropProfilePic',
+  component: CropProfilePic,
+}
+
+export const Default = {
+  args: {
+    src: 'https://i.pravatar.cc/300',
+  },
+}
