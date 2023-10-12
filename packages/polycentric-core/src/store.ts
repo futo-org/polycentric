ea8131101b3741eefacdef579ac0d7ab2e212161
Harpo Roeder <roedeharpo@protonmail.ch>
2023-10-12 03:10:07 +0000

    format

--- packages/polycentric-core/src/store.ts
+++ packages/polycentric-core/src/store.ts
@@ -228,7 +228,7 @@ export class CRDTElementSetIndex {
 
         let result = [];
 
-        for (const [k,] of rows) {
+        for (const [k] of rows) {
             if (!Util.bufferSuffixMatch(k, suffix)) {
                 continue;
             }
