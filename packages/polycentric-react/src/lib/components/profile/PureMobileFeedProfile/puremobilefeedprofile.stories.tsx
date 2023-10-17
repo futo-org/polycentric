0eb6a53cfe94cd340e55ea4eef54a8a84bd48fdc
aidan <aidan@futo.org>
2023-10-17 17:01:12 -0500

    Add profile previews for desktop and mobile

---
+++ packages/polycentric-react/src/lib/components/profile/PureMobileFeedProfile/puremobilefeedprofile.stories.tsx
@@ -0,0 +1,18 @@
+import { PureMobileFeedProfile } from '.'
+
+export default {
+  title: 'Profile/PureMobileFeedProfile',
+  component: PureMobileFeedProfile,
+}
+
+export const Default = {
+  args: {
+    profile: {
+      name: 'John Doe',
+      avatarURL: 'https://i.pravatar.cc/300',
+      description: 'i like to repair. i like to repair. i like to repair. ',
+      followerCount: 100,
+      followingCount: 100,
+    },
+  },
+}
--- packages/polycentric-react/src/lib/components/profile/PureSidebarProfile/index.tsx
+++ packages/polycentric-react/src/lib/components/profile/PureSidebarProfile/index.tsx
@@ -1,10 +1,36 @@
-import { Profile } from '../../../types/profile'
+export interface PureSidebarProfileData {
+  name?: string
+  avatarURL?: string
+  description?: string
+  followerCount?: number
+  followingCount?: number
+  isMyProfile: boolean
+  iAmFollowing?: boolean
+}
 
-export const PureSidebarProfile = ({ profile }: { profile: Profile }) => (
+export const PureSidebarProfile = ({
+  profile,
+  follow,
+  unfollow,
+}: {
+  profile: PureSidebarProfileData
+  follow: () => void
+  unfollow: () => void
+}) => (
   <div className="w-full h-full">
     <div className="flex flex-col items-center justify-center space-y-3">
-      <img src={profile.avatarURL} className="h-24 w-24 rounded-full" />
+      <div className="h-24 w-24 rounded-full border overflow-clip">
+        <img src={profile.avatarURL} className="" />
+      </div>
       <div className="text-2xl font-medium">{profile.name}</div>
+      {profile.isMyProfile && (
+        <button
+          onClick={profile.iAmFollowing ? unfollow : follow}
+          className="bg-blue-500 text-white px-4 py-2 rounded-full"
+        >
+          {profile.iAmFollowing ? 'Unfollow' : 'Follow'}
+        </button>
+      )}
       <div className="flex space-x-2">
         <p>
           <span className="font-bold">{390}</span> followers
