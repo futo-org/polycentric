3f914c10c0a71ed09c8b5f22c39cd4023c511081
Harpo <harpo@futo.org>
2023-11-09 21:43:42 +0000

    Order references by likes / dislkes

--- server/src/queries/query_references.rs
+++ server/src/queries/query_references.rs
@@ -1,7 +1,5 @@
 #[derive(::sqlx::FromRow)]
 struct QueryRow {
-    #[sqlx(try_from = "i64")]
-    id: u64,
     raw_event: ::std::vec::Vec<u8>,
 }
 
@@ -14,13 +12,14 @@ pub(crate) struct QueryResult {
 fn process_rows(
     rows: std::vec::Vec<QueryRow>,
     limit: u64,
+    cursor: &::std::option::Option<u64>,
 ) -> ::anyhow::Result<QueryResult> {
     Ok(QueryResult {
-        cursor: if let Some(last) = rows.last() {
-            if rows.len() == TryInto::<usize>::try_into(limit)? {
-                Some(last.id)
+        cursor: if rows.len() == TryInto::<usize>::try_into(limit)? {
+            if let Some(position) = cursor {
+                Some(position + limit)
             } else {
-                None
+                Some(limit)
             }
         } else {
             None
@@ -34,6 +33,33 @@ fn process_rows(
     })
 }
 
+const LIKES_DISLIKES_QUERY_FRAGMENT: &str = "
+    LEFT JOIN
+        count_lww_element_references_pointer as likes
+    ON
+        events.system_key_type = likes.subject_system_key_type
+    AND
+        events.system_key = likes.subject_system_key
+    AND
+        events.process = likes.subject_process
+    AND
+        events.logical_clock = likes.subject_logical_clock
+    AND
+        likes.value = '\\001'::bytea
+    LEFT JOIN
+        count_lww_element_references_pointer as dislikes
+    ON
+        events.system_key_type = dislikes.subject_system_key_type
+    AND
+        events.system_key = dislikes.subject_system_key
+    AND
+        events.process = dislikes.subject_process
+    AND
+        events.logical_clock = dislikes.subject_logical_clock
+    AND
+        dislikes.value = '\\002'::bytea
+";
+
 pub(crate) async fn query_pointer(
     transaction: &mut ::sqlx::Transaction<'_, ::sqlx::Postgres>,
     system: &crate::model::public_key::PublicKey,
@@ -43,35 +69,34 @@ pub(crate) async fn query_pointer(
     cursor: &::std::option::Option<u64>,
     limit: u64,
 ) -> ::anyhow::Result<QueryResult> {
-    let query = "
+    let query = format!(
+        "
         SELECT
-            id, raw_event
+            events.raw_event as raw_event
         FROM
             events
+        JOIN
+            event_links
+        ON
+            event_links.event_id = events.id
+        {LIKES_DISLIKES_QUERY_FRAGMENT}
         WHERE
-            id
-        IN (
-            SELECT
-                event_id as id
-            FROM
-                event_links
-            WHERE
-                subject_system_key_type = $1
-            AND
-                subject_system_key = $2
-            AND
-                subject_process = $3
-            AND
-                subject_logical_clock = $4
-        )
+            event_links.subject_system_key_type = $1
         AND
-            ($5 IS NULL OR content_type = $5)
+            event_links.subject_system_key = $2
         AND
-            ($6 IS NULL OR id < $6)
+            event_links.subject_process = $3
+        AND
+            event_links.subject_logical_clock = $4
+        AND
+            ($5 IS NULL OR events.content_type = $5)
         ORDER BY
-            id DESC
+            (COALESCE(likes.count, 0) - COALESCE(dislikes.count, 0)) DESC,
+            events.id DESC
+        OFFSET COALESCE($6, 0)
         LIMIT $7
-    ";
+    "
+    );
 
     let from_type_query = if let Some(x) = from_type {
         Some(i64::try_from(*x)?)
@@ -85,7 +110,7 @@ pub(crate) async fn query_pointer(
         None
     };
 
-    let rows = ::sqlx::query_as::<_, QueryRow>(query)
+    let rows = ::sqlx::query_as::<_, QueryRow>(&query)
         .bind(i64::try_from(crate::model::public_key::get_key_type(
             system,
         ))?)
@@ -98,7 +123,7 @@ pub(crate) async fn query_pointer(
         .fetch_all(&mut **transaction)
         .await?;
 
-    process_rows(rows, limit)
+    process_rows(rows, limit, cursor)
 }
 
 pub(crate) async fn query_bytes(
@@ -108,29 +133,28 @@ pub(crate) async fn query_bytes(
     cursor: &::std::option::Option<u64>,
     limit: u64,
 ) -> ::anyhow::Result<QueryResult> {
-    let query = "
+    let query = format!(
+        "
         SELECT
-            id, raw_event
+            events.raw_event as raw_event
         FROM
             events
+        JOIN
+            event_references_bytes
+        ON
+            event_references_bytes.event_id = events.id
+        {LIKES_DISLIKES_QUERY_FRAGMENT}
         WHERE
-            id
-        IN (
-            SELECT
-                event_id as id
-            FROM
-                event_references_bytes
-            WHERE
-                subject_bytes = $1
-        )
+            event_references_bytes.subject_bytes = $1
         AND
-            ($2 IS NULL OR content_type = $2)
-        AND
-            ($3 IS NULL OR id < $3)
+            ($2 IS NULL OR events.content_type = $2)
         ORDER BY
-            id DESC
+            (COALESCE(likes.count, 0) - COALESCE(dislikes.count, 0)) DESC,
+            events.id DESC
+        OFFSET COALESCE($3, 0)
         LIMIT $4
-    ";
+    "
+    );
 
     let from_type_query = if let Some(x) = from_type {
         Some(i64::try_from(*x)?)
@@ -144,7 +168,7 @@ pub(crate) async fn query_bytes(
         None
     };
 
-    let rows = ::sqlx::query_as::<_, QueryRow>(query)
+    let rows = ::sqlx::query_as::<_, QueryRow>(&query)
         .bind(bytes)
         .bind(from_type_query)
         .bind(cursor_query)
@@ -152,7 +176,7 @@ pub(crate) async fn query_bytes(
         .fetch_all(&mut **transaction)
         .await?;
 
-    process_rows(rows, 20)
+    process_rows(rows, 20, cursor)
 }
 
 pub(crate) async fn query_references(
