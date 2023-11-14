1a49cddbab1180ce585dd1bfeffc9e07b2ee9621
aidan <aidan@futo.org>
2023-11-14 00:07:32 -0600

    Add logo to nav bar

--- packages/polycentric-react/src/lib/components/sidebars/mainsidebar/index.tsx
+++ packages/polycentric-react/src/lib/components/sidebars/mainsidebar/index.tsx
@@ -1,10 +1,12 @@
-import { NavLink } from 'react-router-dom'
+import polycentricIcon from '../../../../graphics/icons/favicon.ico'
 import { AccountSwitcher } from '../../profile/AccountSwitcher'
 
 export const MainSidebar = ({ topics }: { topics: string[] }) => (
   <div className="h-full w-full flex flex-col space-y-5 justify-between md:p-5">
-    <div>
-      <h1>Polycentric</h1>
+    <div className="flex flex-col space-y-5">
+      <h1 className="text-lg">
+        <img src={polycentricIcon} className="inline h-[20px]" /> Polycentric
+      </h1>
       <div className="flex flex-col space-y-2 text-right">
         <NavLink to="/">All</NavLink>
         {topics.map((topic) => (
