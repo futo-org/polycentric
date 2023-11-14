9b0099dff34b9ad3930d807d783b801e19c95965
Aidan <33-aidan@users.noreply.gitlab.futo.org>
2023-11-14 21:41:18 +0000

    Ad ionic move setup force ios

--- packages/polycentric-react/src/lib/index.ts
+++ packages/polycentric-react/src/lib/index.ts
@@ -1,8 +1,5 @@
-import { setupIonicReact } from '@ionic/react'
 import './style/ionic.css'
 import './style/tailwind.css'
-
-setupIonicReact({})
 
 export * from './app'
 export * from './components'
