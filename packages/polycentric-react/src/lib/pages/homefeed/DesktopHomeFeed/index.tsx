03b2c999ae73992686faaa9cb65c7d82a1ad4235
aidan <aidan@futo.org>
2023-11-14 00:02:05 -0600

    Add explore to desktop home feed

--- packages/polycentric-react/src/lib/pages/homefeed/DesktopHomeFeed/index.tsx
+++ packages/polycentric-react/src/lib/pages/homefeed/DesktopHomeFeed/index.tsx
@@ -1,7 +1,9 @@
+import { Compose } from '../../../components'
 import { InfiniteScrollWithRightCol } from '../../../components/layout/infinitescrollwithrightcol'
 import { useExploreFeed } from '../../../hooks/feedHooks'
 
 export const DesktopHomeFeed = () => {
   const [data, advanceFeed] = useExploreFeed()
   const compose = <Compose hideTopic={true} />
+  return <InfiniteScrollWithRightCol data={data} advanceFeed={advanceFeed} leftCol={null} topComponent={compose} />
 }
