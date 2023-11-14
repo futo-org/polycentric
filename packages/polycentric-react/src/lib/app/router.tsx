137dc3b8c45405862163103e65e2c0f1bac9f17c
aidan <aidan@futo.org>
2023-11-14 00:01:29 -0600

    Switch router to declarative object for preparation for addt stack router

--- packages/polycentric-react/src/lib/app/router.tsx
+++ packages/polycentric-react/src/lib/app/router.tsx
@@ -1,35 +1,62 @@
-import { IonPage } from '@ionic/react'
+import { IonContent, IonNav, IonPage } from '@ionic/react'
+import { useCallback, useRef } from 'react'
 import { Route as RouterRoute } from 'react-router-dom'
 import { HomeFeedPage } from '../pages/homefeed'
+import { PostFeedPage } from '../pages/postfeed'
 import { UserFeedPage } from '../pages/userfeed'
 
 export const Route = ({
-  children,
+  Component,
   path,
-  allowMobileDrawer = false,
+  rootPath = false,
 }: {
-  children: React.ReactNode
+  Component: Page
   path?: string
-  allowMobileDrawer?: boolean
-}) => (
-  <IonPage className="bg-white" id={allowMobileDrawer ? 'main-drawer' : undefined}>
-    <RouterRoute path={path}>{children}</RouterRoute>
-  </IonPage>
-)
+  rootPath?: boolean
+}) => {
+  const navref = useRef<HTMLIonNavElement>(null)
+
+  // id={allowMobileDrawer ? 'main-drawer' : undefined
+  const root = useCallback(() => {
+    return (
+      <IonContent>
+        <Component />
+      </IonContent>
+    )
+  }, [Component])
+
+  if (rootPath) {
+    return (
+      <RouterRoute path={path} exact={true}>
+        <IonPage>
+          <IonNav root={root} ref={navref} />
+        </IonPage>
+      </RouterRoute>
+    )
+  }
+
+  return (
+    <RouterRoute path={path} exact={true}>
+      <IonNav root={root} ref={navref} />
+    </RouterRoute>
+  )
+}
+
+export type Page = React.ComponentType<{ memoryPath?: string }>
+type RouteData = Record<string, { component: Page; root?: boolean }>
+
+export const routeData: RouteData = {
+  '/': { component: HomeFeedPage, root: true },
+  '/t/:topic': { component: HomeFeedPage },
+  '/user/:urlInfoString': { component: UserFeedPage },
+  '/post/:urlInfoString/': { component: PostFeedPage },
+  '/post/:urlInfoString/:id': { component: PostFeedPage },
+}
 
 export const AppRouter = () => (
   <>
-    <Route path="/" allowMobileDrawer={true}>
-      <HomeFeedPage />
-    </Route>
-    <Route path="/t/:topic">
-      <p>wip</p>
-    </Route>
-    <Route path="/user/:urlInfoString">
-      <UserFeedPage />
-    </Route>
-    {/* <Route path="/:urlinfo">
-      <URLInfoRouter />
-    </Route> */}
+    {Object.entries(routeData).map(([path, { component, root }]) => (
+      <Route path={path} key={path} rootPath={root} Component={component} />
+    ))}
   </>
 )
