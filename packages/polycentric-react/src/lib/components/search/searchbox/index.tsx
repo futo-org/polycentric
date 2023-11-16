940175a7f115ba5dceeeb844f610282b253831a6
aidan <aidan@futo.org>
2023-11-16 14:06:38 -0600

    Add go button to search box

--- packages/polycentric-react/src/lib/components/search/searchbox/index.tsx
+++ packages/polycentric-react/src/lib/components/search/searchbox/index.tsx
@@ -1,3 +1,4 @@
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
 import { useDebounce } from '@uidotdev/usehooks'
 import { useEffect, useState } from 'react'
 import { Link } from '../../util/link'
@@ -26,13 +27,23 @@ export const SearchBox = ({
 
   return (
     <div className="flex flex-col space-y-2">
-      <input
-        type="text"
-        placeholder="Search..."
-        className="rounded-full border text-xl p-2 pl-5 font-light placeholder:text-gray-300"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      <div className="flex rounded-full border focus-within:border-gray-300 p-2 space-x-2">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="flex-grow text-lg text-gray-900 ml-4 font-light placeholder:text-gray-300 focus:outline-none"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Link
+          className={`rounded-full border aspect-square h-[2.5rem] w-[2.5rem] flex justify-center items-center ${
+            query !== '' ? 'hover:bg-gray-50' : ''
+          }`}
+          routerLink={`/search/${query}`}
+        >
+          <ArrowRightIcon className={`w-6 h-6  ${query === '' ? 'text-gray-300' : 'text-gray-500'}`} />
+        </Link>
+      </div>
       {query.length > 0 && results && (
         <div className="relative">
           <div className="flex flex-col space-y-0 border rounded-lg bg-white absolute w-full">
