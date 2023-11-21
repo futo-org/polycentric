92eab69bc741f0c73319cb2f696fc31badad249a
Aidan <33-aidan@users.noreply.gitlab.futo.org>
2023-11-21 19:36:47 +0000

    Appease linter

--- packages/polycentric-react/src/lib/components/onboarding/onboarding/index.tsx
+++ packages/polycentric-react/src/lib/components/onboarding/onboarding/index.tsx
@@ -109,17 +109,6 @@ const GenCredsPanelItem = ({
   </div>
 )
 
-// copy this but for a profile image upload, with a small circle with an upload symbol (just put "u" fo for now) that switches to the uploaded image and an x that appears next to it to remove it
-const XIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-    <path
-      fillRule="evenodd"
-      d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-      clipRule="evenodd"
-    />
-  </svg>
-)
-
 const useCleanupObjectURL = (url?: string) => {
   useEffect(() => {
     return () => {
