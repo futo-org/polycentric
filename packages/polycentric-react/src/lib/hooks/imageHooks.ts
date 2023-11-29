83e526ad9a7bac9f2f59282ac27f38f657c80ce4
aidan <aidan@futo.org>
2023-11-29 17:13:23 -0600

    Add caching to useBlobDisplayURL hook

--- packages/polycentric-react/src/lib/hooks/imageHooks.ts
+++ packages/polycentric-react/src/lib/hooks/imageHooks.ts
@@ -1,6 +1,48 @@
 import { Models, Protocol } from '@polycentric/polycentric-core'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { useBlobQuery } from './queryHooks'
+
+const urlCache: Map<Blob, { url: string; count: number; timeoutId?: number }> = new Map()
+
+export const useBlobDisplayURL = (blob?: Blob): string | undefined => {
+  const cacheEntry = useRef<{ url: string; count: number; timeoutId?: number } | null>(null)
+  const [blobURL, setBlobURL] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    if (blob) {
+      let entry = urlCache.get(blob)
+      if (!entry) {
+        const url = URL.createObjectURL(blob)
+        entry = { url, count: 1 }
+        urlCache.set(blob, entry)
+      } else {
+        entry.count += 1
+        if (entry.timeoutId) {
+          clearTimeout(entry.timeoutId)
+          entry.timeoutId = undefined
+        }
+      }
+      cacheEntry.current = entry
+      setBlobURL(entry.url)
+    } else {
+      setBlobURL(undefined)
+    }
+
+    return () => {
+      if (cacheEntry.current && blob) {
+        cacheEntry.current.count -= 1
+        if (cacheEntry.current.count === 0) {
+          cacheEntry.current.timeoutId = window.setTimeout(() => {
+            if (cacheEntry.current?.url) URL.revokeObjectURL(cacheEntry.current.url)
+            urlCache.delete(blob)
+          }, 10000)
+        }
+      }
+    }
+  }, [blob])
+
+  return blobURL
+}
 
 export const useImageManifestDisplayURL = (
   system?: Models.PublicKey.PublicKey,
