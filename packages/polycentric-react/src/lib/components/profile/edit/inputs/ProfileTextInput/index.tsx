8ece7d63ad9de3bef6d667dc69c3297f19090966
aidan <aidan@futo.org>
2023-11-28 20:20:12 -0600

    Change profile text inputs to accept any input HTML attribute

--- packages/polycentric-react/src/lib/components/profile/edit/inputs/ProfileTextInput/index.tsx
+++ packages/polycentric-react/src/lib/components/profile/edit/inputs/ProfileTextInput/index.tsx
@@ -1,12 +1,10 @@
+import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react'
 import { GrowingTextArea } from '../../../../util/input/GrowingTextArea'
 
 export const ProfileTextInput = ({
-  value,
-  onChange,
   title,
   hint,
-  autoComplete,
-  readOnly = false,
+  ...rest
 }: {
   value?: string
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
@@ -14,36 +12,24 @@ export const ProfileTextInput = ({
   hint?: string
   autoComplete?: string
   readOnly?: boolean
-}) => (
+} & InputHTMLAttributes<HTMLInputElement>) => (
   <div className="flex flex-col gap-y-1">
     <h3 className="font-medium">{title}</h3>
-    <input
-      type="text"
-      className="rounded-full border text-lg py-2.5 px-4 h-[3.15rem]"
-      autoComplete={autoComplete}
-      readOnly={readOnly}
-      value={value}
-      onChange={onChange}
-    />
+    <input type="text" className="rounded-full border text-lg py-2.5 px-4 h-[3.15rem]" {...rest} />
     <p className="text-sm text-gray-700">{hint}</p>
   </div>
 )
 
 export const ProfileTextArea = ({
-  value,
-  onChange,
   title,
   hint,
-  autoComplete,
-  readOnly = false,
+  ...rest
 }: {
   value?: string
   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
   title: string
   hint?: string
-  autoComplete?: string
-  readOnly?: boolean
-}) => (
+} & TextareaHTMLAttributes<HTMLTextAreaElement>) => (
   <div className="flex flex-col gap-y-1">
     <h3 className="font-medium">{title}</h3>
     <div className="rounded-[1.575rem] border focus-within:border-gray-300 overflow-clip">
@@ -51,10 +37,7 @@ export const ProfileTextArea = ({
         // 3.15rem / 2 = 1.575rem
         maxHeightPx={108}
         className="text-lg py-3 px-4 w-full h-full focus:outline-none"
-        autoComplete={autoComplete}
-        readOnly={readOnly}
-        value={value}
-        onChange={onChange}
+        {...rest}
       />
     </div>
     <p className="text-sm text-gray-700">{hint}</p>
