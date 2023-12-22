63ec10ab5654109a0559e2e88c786aca4228c1ca
aidan <aidan@futo.org>
2023-12-21 20:30:52 -0600

    Add source code to desktop

--- packages/polycentric-react/src/lib/components/layout/rightcol/index.tsx
+++ packages/polycentric-react/src/lib/components/layout/rightcol/index.tsx
@@ -31,13 +31,16 @@ vw-1536px)/2)] "
               </div>
               {leftCol}
             </div>
-            <div className="p-5 w-full text-right">
+            <div className="p-5 w-full text-right text-gray-400 text-sm">
               <a
-                href="https://docs.polycentric.io/privacy-policy/"
+                href="https://gitlab.futo.org/polycentric/polycentric"
                 target="_blank"
                 rel="noreferrer"
-                className="text-gray-400 text-sm"
+                className="block"
               >
+                Source Code
+              </a>
+              <a href="https://docs.polycentric.io/privacy-policy/" target="_blank" rel="noreferrer" className="block">
                 Privacy Policy
               </a>
             </div>
