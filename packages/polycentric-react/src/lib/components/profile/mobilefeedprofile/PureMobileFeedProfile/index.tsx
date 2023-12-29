c8e8020fc70bb07f6ffae676e7cd6b1226c389f6
Harpo Roeder <roederharpo@protonmail.ch>
2023-12-29 16:51:00 -0600

    lint

--- packages/polycentric-react/src/lib/components/profile/mobilefeedprofile/PureMobileFeedProfile/index.tsx
+++ packages/polycentric-react/src/lib/components/profile/mobilefeedprofile/PureMobileFeedProfile/index.tsx
@@ -3,23 +3,6 @@ import { useState } from 'react'
 import { EditProfileActions, PureEditProfile } from '../../edit/PureEditProfile'
 import { PureSidebarProfileData } from '../../sidebarprofile/PureSidebarProfile'
 
-const ElipsesIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-6 h-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-    />
-  </svg>
-)
-
 export const PureMobileFeedProfile = ({
   profile,
   follow,
