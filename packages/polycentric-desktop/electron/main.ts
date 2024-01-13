7a246aec1d1ffdba1689a3d9e2ebfc990133fd35
Harpo <harpo@futo.org>
2024-01-13 16:35:57 +0000

    Fix desktop build

---
+++ packages/polycentric-desktop/electron/main.ts
@@ -0,0 +1,50 @@
+import * as Electron from 'electron';
+import * as ElectronIsDev from 'electron-is-dev';
+import * as Path from 'path';
+
+const createMainWindow = () => {
+    let mainWindow = new Electron.BrowserWindow({
+        width: Electron.screen.getPrimaryDisplay().workArea.width,
+        height: Electron.screen.getPrimaryDisplay().workArea.height,
+        show: false,
+        backgroundColor: 'white',
+        webPreferences: {
+            nodeIntegration: true,
+            devTools: true,
+            contextIsolation: false,
+        },
+    });
+
+    const startURL = ElectronIsDev
+        ? 'http://localhost:3000'
+        : `file://${Path.join(__dirname, '../build/index.html')}`;
+
+    mainWindow.loadURL(startURL);
+
+    mainWindow.once('ready-to-show', () => mainWindow.show());
+
+    mainWindow.on('closed', () => {
+        // mainWindow = null;
+    });
+
+    mainWindow.webContents.on('new-window', (event, url) => {
+        event.preventDefault();
+        mainWindow.loadURL(url);
+    });
+};
+
+Electron.app.whenReady().then(() => {
+    createMainWindow();
+
+    Electron.app.on('activate', () => {
+        if (!Electron.BrowserWindow.getAllWindows().length) {
+            createMainWindow();
+        }
+    });
+});
+
+Electron.app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        Electron.app.quit();
+    }
+});
