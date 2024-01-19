025de06313f58e1813c6b8e74972e12f78cf797e
aidan <aidan@futo.org>
2024-01-19 10:27:17 -0600

    Redo profile picture so the loading image is overlayed with the placeholder instead of replacing it

--- packages/polycentric-react/src/lib/components/profile/ProfilePicture/index.tsx
+++ packages/polycentric-react/src/lib/components/profile/ProfilePicture/index.tsx
@@ -2,17 +2,26 @@ export const ProfilePicture = ({
     src,
     alt,
     className,
+    showPlaceholder = false,
 }: {
     src?: string | undefined;
     alt?: string;
     className: string;
+    showPlaceholder?: boolean;
 }) => {
     return (
-        <div className={`rounded-full overflow-clip border ${className}`}>
+        <div
+            className={`rounded-full overflow-clip border ${className} relative`}
+        >
             {src === undefined ? (
                 <div className="w-full h-full bg-gray-200"></div>
             ) : (
-                <img className="w-full h-full" src={src} alt={alt} />
+                <>
+                    <img className="w-full h-full" src={src} alt={alt} />
+                    {showPlaceholder && (
+                        <div className="absolute inset-0 w-full h-full bg-gray-200"></div>
+                    )}
+                </>
             )}
         </div>
     );
