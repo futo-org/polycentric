190136ed121c83ed0ee334a7733224c2fd29aedd
Harpo <harpo@futo.org>
2024-01-19 03:04:17 +0000

    Profile Image Single Flight And Cache With Timeout

--- packages/polycentric-react/src/lib/hooks/utilHooks.ts
+++ packages/polycentric-react/src/lib/hooks/utilHooks.ts
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import * as RXJS from 'rxjs';
 
 export const useDebouncedEffect = (
     effect: () => void,
@@ -12,3 +13,93 @@ export const useDebouncedEffect = (
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, deps);
 };
+
+export type ObservableCacheItem<T> = {
+    value: T | undefined;
+    callbacks: Set<(value: T) => void>;
+    unsubscribe: () => void;
+    timeoutMilliseconds: number;
+    generation: number;
+};
+
+export function useObservableWithCache<T>(
+    cache: Map<string, ObservableCacheItem<T>>,
+    cacheKey: string,
+    cacheTimeoutMilliseconds: number,
+    observable: RXJS.Observable<T>,
+): T | undefined {
+    const [state, setState] = useState<T | undefined>(() => {
+        const item = cache.get(cacheKey);
+        return item ? item.value : undefined;
+    });
+
+    useEffect(() => {
+        const potentialItem = cache.get(cacheKey);
+
+        if (potentialItem) {
+            if (state !== potentialItem.value) {
+                setState(potentialItem.value);
+            }
+        } else if (state !== undefined) {
+            setState(undefined);
+        }
+    }, [cache, cacheKey, state]);
+
+    useEffect(() => {
+        const potentialItem = cache.get(cacheKey);
+
+        const item: ObservableCacheItem<T> = potentialItem
+            ? potentialItem
+            : {
+                  value: undefined,
+                  callbacks: new Set(),
+                  unsubscribe: () => {},
+                  timeoutMilliseconds: cacheTimeoutMilliseconds,
+                  generation: 0,
+              };
+
+        const cb = (value: T) => {
+            setState(value);
+        };
+
+        item.generation++;
+        item.callbacks.add(cb);
+
+        if (potentialItem === undefined) {
+            cache.set(cacheKey, item);
+
+            const subscription = observable.subscribe((value: T) => {
+                item.value = value;
+                item.callbacks.forEach((cb) => cb(value));
+            });
+
+            item.unsubscribe = () => {
+                subscription.unsubscribe();
+            };
+        }
+
+        const finalize = (initial: boolean, generation: number) => {
+            if (item.callbacks.size === 0) {
+                if (
+                    item.timeoutMilliseconds === 0 ||
+                    (item.generation === generation && !initial)
+                ) {
+                    item.unsubscribe();
+                    cache.delete(cacheKey);
+                } else {
+                    setTimeout(() => {
+                        finalize(false, item.generation);
+                    }, item.timeoutMilliseconds);
+                }
+            }
+        };
+
+        return () => {
+            item.callbacks.delete(cb);
+
+            finalize(true, item.generation);
+        };
+    }, [cache, cacheKey, cacheTimeoutMilliseconds, observable]);
+
+    return state;
+}
