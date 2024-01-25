9fa53d096e7aa1c670ef83b146bb56861dde5aa2
Harpo Roeder <roedeharpo@protonmail.ch>
2024-01-25 10:32:28 +0000

    full initial query-crdt2

--- packages/polycentric-core/src/queries/query-crdt2.ts
+++ packages/polycentric-core/src/queries/query-crdt2.ts
@@ -9,13 +9,15 @@ import * as QueryHead from './query-head2';
 import { QueryEvent, queryEventObservable } from './query-event2';
 import { UnregisterCallback, DuplicatedCallbackError } from './shared';
 
-export type SuccessCallback = (value: Uint8Array) => void;
+export type SuccessCallback = (value: Uint8Array | undefined) => void;
+
+type Box<T> = { value: T };
 
 type StateForCRDT = {
-    value: Uint8Array;
-    unixMilliseconds: Long;
+    value: Box<Uint8Array | undefined>;
     readonly callbacks: Set<SuccessCallback>;
-    fulfilled: boolean;
+    fulfilled: Box<boolean>;
+    unsubscribe: () => void;
 };
 
 type StateForSystem = {
@@ -88,14 +90,11 @@ export class QueryCRDT extends HasUpdate {
         this.queryEvent = queryEvent;
     }
 
-    private run(
+    private pipeline(
         system: Models.PublicKey.PublicKey,
         contentType: Models.ContentType.ContentType,
-    ): void {
-        const observable = QueryHead.queryHeadObservable(
-            this.queryHead,
-            system,
-        ).pipe(
+    ): RXJS.Observable<Uint8Array | undefined> {
+        return QueryHead.queryHeadObservable(this.queryHead, system).pipe(
             RXJS.switchMap((head) =>
                 RXJS.combineLatest(
                     mapToArray(head, (signedEvent) => {
@@ -148,30 +147,64 @@ export class QueryCRDT extends HasUpdate {
 
         const contentTypeString = Models.ContentType.toString(contentType);
 
+        let initial = true;
+
         const stateForCRDT: StateForCRDT = Util.lookupWithInitial(
             stateForSystem.state,
             contentTypeString,
             () => {
+                initial = false;
+
+                const value: Box<Uint8Array | undefined> = { value: undefined };
+                const fulfilled: Box<boolean> = { value: false };
+                const callbacks = new Set([callback]);
+
+                const subscription = this.pipeline(
+                    system,
+                    contentType,
+                ).subscribe((updatedValue) => {
+                    value.value = updatedValue;
+                    fulfilled.value = true;
+
+                    callbacks.forEach((cb) => cb(value.value));
+                });
+
                 return {
-                    value: new Uint8Array(),
-                    unixMilliseconds: Long.UZERO,
-                    callbacks: new Set(),
-                    fulfilled: false,
+                    value: value,
+                    callbacks: callbacks,
+                    fulfilled: fulfilled,
+                    unsubscribe: () => {
+                        subscription.unsubscribe();
+                    },
                 };
             },
         );
 
-        if (stateForCRDT.callbacks.has(callback)) {
-            throw DuplicatedCallbackError;
+        if (!initial) {
+            if (stateForCRDT.callbacks.has(callback)) {
+                throw DuplicatedCallbackError;
+            }
+
+            stateForCRDT.callbacks.add(callback);
+
+            if (stateForCRDT.fulfilled.value) {
+                callback(stateForCRDT.value.value);
+            }
         }
 
-        stateForCRDT.callbacks.add(callback);
+        return () => {
+            stateForCRDT.callbacks.delete(callback);
 
-        if (stateForCRDT.fulfilled) {
-            callback(stateForCRDT.value);
-        }
+            if (stateForCRDT.callbacks.size === 0) {
+                stateForCRDT.unsubscribe();
 
-        return () => {};
+                stateForSystem.state.delete(contentTypeString);
+
+                if (stateForSystem.state.size === 0) {
+                    this.state.delete(systemString);
+                }
+            }
+        };
     }
 
     public update(signedEvent: Models.SignedEvent.SignedEvent): void {}
