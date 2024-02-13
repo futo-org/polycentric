570c99041e67cf3fd8c8968d4c77a0de0127b73b
Harpo <harpo@futo.org>
2024-02-13 20:42:17 +0000

    polycentric-server /head include system processes

--- server/src/handlers/get_head.rs
+++ server/src/handlers/get_head.rs
@@ -12,21 +12,67 @@ pub(crate) async fn handler(
     state: ::std::sync::Arc<crate::State>,
     query: Query,
 ) -> Result<Box<dyn ::warp::Reply>, ::std::convert::Infallible> {
-    let mut transaction = crate::warp_try_err_500!(state.pool.begin().await);
+    Ok(crate::warp_try_err_500!(handler_inner(state, query).await))
+}
+
+async fn handler_inner(
+    state: ::std::sync::Arc<crate::State>,
+    query: Query,
+) -> ::anyhow::Result<Box<dyn ::warp::Reply>> {
+    let mut transaction = state.pool.begin().await?;
+
+    let head_signed_events =
+        crate::postgres::load_system_head(&mut transaction, &query.system)
+            .await?;
+
+    let mut result_signed_events = head_signed_events.clone();
+
+    for head_signed_event in head_signed_events.into_iter() {
+        let head_event =
+            crate::model::event::from_vec(head_signed_event.event())?;
+
+        if *head_event.content_type()
+            == crate::model::known_message_types::SYSTEM_PROCESSES
+        {
+            continue;
+        }
+
+        let previous_system_processes_index = head_event
+            .indices()
+            .indices
+            .clone()
+            .into_iter()
+            .find(|index| {
+                index.index_type
+                    == crate::model::known_message_types::SYSTEM_PROCESSES
+            });
+
+        if let Some(index) = previous_system_processes_index {
+            let previous_system_processes_signed_event =
+                crate::postgres::load_event(
+                    &mut transaction,
+                    head_event.system(),
+                    head_event.process(),
+                    index.logical_clock,
+                )
+                .await?;
+
+            if let Some(event) = previous_system_processes_signed_event {
+                result_signed_events.push(event);
+            }
+        }
+    }
+
+    transaction.commit().await?;
 
     let mut result = crate::protocol::Events::new();
 
-    result.events = crate::warp_try_err_500!(
-        crate::postgres::load_system_head(&mut transaction, &query.system,)
-            .await
-    )
-    .iter()
-    .map(crate::model::signed_event::to_proto)
-    .collect();
+    result.events = result_signed_events
+        .iter()
+        .map(crate::model::signed_event::to_proto)
+        .collect();
 
-    crate::warp_try_err_500!(transaction.commit().await);
-
-    let result_serialized = crate::warp_try_err_500!(result.write_to_bytes());
+    let result_serialized = result.write_to_bytes()?;
 
     Ok(Box::new(::warp::reply::with_header(
         ::warp::reply::with_status(
