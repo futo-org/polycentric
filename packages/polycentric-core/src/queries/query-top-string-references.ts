09d82589433288ec9fdf6600428fd31323f2c0d9
aidan <aidan@futo.org>
2024-04-16 16:25:52 -0500

    Rewrite QueryTopStringReferences query

--- packages/polycentric-core/src/queries/query-top-string-references.ts
+++ packages/polycentric-core/src/queries/query-top-string-references.ts
@@ -3,6 +3,7 @@ import * as RXJS from 'rxjs';
 import * as APIMethods from '../api-methods';
 import * as Models from '../models';
 import * as ProcessHandle from '../process-handle';
+import { fromPromiseExceptionToEmpty } from '../util';
 import * as QueryServers from './query-servers';
 
 export type Callback = (topReferences: Models.AggregationBucket.Type[]) => void;
@@ -29,56 +30,37 @@ export class QueryTopStringReferences {
             this.queryServers,
             this.processHandle.system(),
         )
-            // Only do this once, this is used for search and we can't have pop-in
-            .pipe(RXJS.first())
-            .subscribe((servers) => {
-                const requestObservables = [...servers].map((server) => {
-                    return new RXJS.Observable<Models.ResultTopStringReferences.Type>(
-                        (subscriber) => {
-                            void APIMethods.getTopStringReferences(
-                                server,
-                                query,
-                            ).then((buckets) => {
-                                subscriber.next(buckets);
-                                subscriber.complete();
-                            });
-                        },
-                    ).pipe(
-                        RXJS.timeout({ first: timeoutMS }),
-                        RXJS.catchError(() => RXJS.EMPTY),
-                    );
-                });
-
-                RXJS.forkJoin(requestObservables).subscribe((responses) => {
-                    // for each key, add up all the values and return the top 10
+            .pipe(
+                RXJS.first(),
+                RXJS.switchMap((servers) => {
+                    const requestObservables = [...servers].map((server) => {
+                        return fromPromiseExceptionToEmpty(
+                            APIMethods.getTopStringReferences(server, query),
+                        )
+                        // .pipe(RXJS.timeout({ first: timeoutMS }));
+                    });
+                    return RXJS.forkJoin(requestObservables);
+                }),
+                RXJS.map((responses) => {
+                    console.log(responses);
                     const topReferences = new Map<string, number>();
-
-                    for (const response of responses) {
-                        for (const bucket of response.buckets) {
+                    responses.forEach((response) => {
+                        response.buckets.forEach((bucket) => {
                             const count = topReferences.get(bucket.key) ?? 0;
                             topReferences.set(bucket.key, count + bucket.value);
-                        }
-                    }
-
-                    const sorted = [...topReferences.keys()].sort((a, b) => {
-                        // these coalescence ops are useless because we know the keys exist, but eslint doesn't
-                        const topA = topReferences.get(a) ?? 0;
-                        const topB = topReferences.get(b) ?? 0;
-                        return topB - topA;
+                        });
                     });
-
-                    const topBuckets = sorted.slice(0, 10).map((key) => {
-                        return {
+                    return [...topReferences.entries()]
+                        .sort((a, b) => b[1] - a[1])
+                        .slice(0, 10)
+                        .map(([key, value]) => ({
                             key,
-                            value: Math.ceil(
-                                (topReferences.get(key) ?? 0) /
-                                    responses.length,
-                            ),
-                        };
-                    });
-
-                    callback(topBuckets);
-                });
+                            value: Math.ceil(value / responses.length),
+                        }));
+                }),
+            )
+            .subscribe((topBuckets) => {
+                callback(topBuckets);
             });
     }
 }
