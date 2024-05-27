090697f541847b49e4749b7e3ebb6fee0e3ea5d1
Harpo Roeder <roederharpo@protonmail.ch>
2024-05-27 13:34:02 -0500

    sql from file

--- server/src/queries/insert_event_batch.rs
+++ server/src/queries/insert_event_batch.rs
@@ -41,86 +41,9 @@ struct ResultRow2 {
 pub (crate) async fn prepare(
     transaction: &::deadpool_postgres::Transaction<'_>,
 ) -> ::anyhow::Result<::tokio_postgres::Statement> {
-    let query = "
-        INSERT INTO events
-        (
-            system_key_type,
-            system_key,
-            process,
-            logical_clock,
-            content_type,
-            content,
-            vector_clock,
-            indices,
-            signature,
-            raw_event,
-            server_time,
-            unix_milliseconds
-        )
-        SELECT * FROM UNNEST (
-            $1::bigint[],
-            $2::bytea[],
-            $3::bytea[],
-            $4::bigint[],
-            $5::bigint[],
-            $6::bytea[],
-            $7::bytea[],
-            $8::bytea[],
-            $9::bytea[],
-            $10::bytea[],
-            $11::bigint[],
-            $12::bigint[]
-        ) as p (
-            system_key_type,
-            system_key,
-            process,
-            logical_clock,
-            content_type,
-            content,
-            vector_clock,
-            indices,
-            signature,
-            raw_event,
-            server_time,
-            unix_milliseconds
-        )
-        WHERE (
-            system_key_type,
-            system_key,
-            process,
-            logical_clock
-        ) NOT IN (
-            SELECT
-                system_key_type,
-                system_key,
-                process,
-                logical_clock
-            FROM
-                deletions
-            WHERE
-                deletions.system_key_type = system_key_type
-            AND
-                deletions.system_key = system_key
-            AND
-                deletions.process = process
-            AND
-                deletions.logical_clock = logical_clock
-        )
-        ORDER BY
-            system_key_type,
-            system_key,
-            process,
-            logical_clock
-        ON CONFLICT DO NOTHING
-        RETURNING
-            id,
-            system_key_type,
-            system_key,
-            process,
-            logical_clock;
-    ";
-
-    let statement = transaction.prepare_cached(query).await?;
+    let statement = transaction.prepare_cached(
+        ::std::include_str!("../sql/insert_event_batch.sql"),
+    ).await?;
 
     Ok(statement)
 }
--- server/src/sql/insert_event_batch.sql
+++ server/src/sql/insert_event_batch.sql
@@ -1,4 +1,5 @@
-INSERT INTO events (
+INSERT INTO events
+(
     system_key_type,
     system_key,
     process,
@@ -12,9 +13,21 @@ INSERT INTO events (
     server_time,
     unix_milliseconds
 )
-SELECT *
-FROM
-    UNNEST($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) AS p (
+SELECT * FROM
+    UNNEST(
+        $1::bigint [],
+        $2::bytea [],
+        $3::bytea [],
+        $4::bigint [],
+        $5::bigint [],
+        $6::bytea [],
+        $7::bytea [],
+        $8::bytea [],
+        $9::bytea [],
+        $10::bytea [],
+        $11::bigint [],
+        $12::bigint []
+    ) AS p (
         system_key_type,
         system_key,
         process,
@@ -28,29 +41,36 @@ FROM
         server_time,
         unix_milliseconds
     )
-WHERE
-    (system_key_type, system_key, process, logical_clock)
-    NOT IN (
-        SELECT
-            system_key_type,
-            system_key,
-            process,
-            logical_clock
-        FROM deletions
-        WHERE
-            deletions.system_key_type = system_key_type
-            AND deletions.system_key = system_key
-            AND deletions.process = process
-            AND deletions.logical_clock = logical_clock
-    )
+WHERE (
+    system_key_type,
+    system_key,
+    process,
+    logical_clock
+) NOT IN (
+    SELECT
+        system_key_type,
+        system_key,
+        process,
+        logical_clock
+    FROM
+        deletions
+    WHERE
+        deletions.system_key_type = system_key_type
+        AND
+        deletions.system_key = system_key
+        AND
+        deletions.process = process
+        AND
+        deletions.logical_clock = logical_clock
+)
 ORDER BY
     system_key_type,
     system_key,
     process,
     logical_clock
-ON CONFLICT
-DO NOTHING
-RETURNING id,
+ON CONFLICT DO NOTHING
+RETURNING
+id,
 system_key_type,
 system_key,
 process,
