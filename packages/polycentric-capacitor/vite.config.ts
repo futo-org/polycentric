b1c0e586f16b02e9412d5d5252a7101f31975ead
Aidan <aidan@futo.org>
2024-06-10 15:57:58 -0500

    Update vite.config.ts to remove vite-plugin-legacy

--- packages/polycentric-capacitor/vite.config.ts
+++ packages/polycentric-capacitor/vite.config.ts
@@ -1,9 +1,6 @@
-import legacy from '@vitejs/plugin-legacy'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    legacy()
-  ]
-})
+    plugins: [],
+});
