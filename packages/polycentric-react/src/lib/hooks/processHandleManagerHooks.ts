7dfbab11939c3768332afd2e96b5d67c25141bcf
Aidan <aidan@futo.org>
2024-08-16 12:22:46 -0700

    Lint fix for processHandleManager

--- packages/polycentric-react/src/lib/hooks/processHandleManagerHooks.ts
+++ packages/polycentric-react/src/lib/hooks/processHandleManagerHooks.ts
@@ -222,7 +222,7 @@ export function useProcessHandleManagerBaseComponentHook(
             const stores = await metaStore.listStores();
             setStores(stores);
         },
-        [metaStore, internalHookState.activeStore],
+        [metaStore, internalHookState.activeStore, changeHandle],
     );
 
     useEffect(() => {
