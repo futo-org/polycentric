f18366fee055e3548cbd1a1ffb457f3c031bd7ac
Aidan <aidan@futo.org>
2024-10-01 17:07:12 -0700

    continue making csam optional

--- server/src/moderation/providers/csam/mod.rs
+++ server/src/moderation/providers/csam/mod.rs
@@ -8,16 +8,20 @@ pub mod photodna;
 pub async fn make_provider(
     config: &Config,
 ) -> ::anyhow::Result<Box<dyn ModerationCSAMProvider>> {
-    let interface = config.csam_interface.clone();
-    let mut provider = match interface.as_str() {
-        "photodna" => Box::new(photodna::PhotoDNAProvider::new()),
-        _ => {
-            return Err(anyhow::anyhow!(
-                "Unknown provider: {}",
-                interface
-            ))
+    match &config.csam_interface {
+        Some(interface) => {
+            let mut provider = match interface.as_str() {
+                "photodna" => Box::new(photodna::PhotoDNAProvider::new()),
+                _ => {
+                    return Err(anyhow::anyhow!(
+                        "Unknown provider: {}",
+                        interface
+                    ))
+                }
+            };
+            provider.init(config).await?;
+            Ok(provider)
         }
-    };
-    provider.init(config).await?;
-    Ok(provider)
+        None => Err(anyhow::anyhow!("CSAM interface not set")),
+    }
 }
