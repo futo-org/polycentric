a6272ec404f88fb4feb0fd64f18a690e11248169
Aidan <aidan@futo.org>
2024-10-01 19:54:59 -0700

    make self harm part of violence

--- server/src/moderation/moderation_queue.rs
+++ server/src/moderation/moderation_queue.rs
@@ -1,6 +1,7 @@
 use crate::moderation::providers;
 use futures::stream::{self, StreamExt};
 use log::debug;
+use polycentric_protocol::model::moderation_tag::ModerationTagName;
 use protobuf::Message;
 use std::sync::Arc;
 use tokio::sync::Semaphore;
