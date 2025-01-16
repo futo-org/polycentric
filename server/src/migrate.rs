72fb494c49beff9e1dd71945012019049d77f7c4
Aidan <aidan@futo.org>
2025-01-16 12:02:32 -0800

    clean up remove migration

--- server/src/migrate.rs
+++ server/src/migrate.rs
@@ -1,6 +1,5 @@
 use crate::moderation::ModerationOptions;
 use ::protobuf::Message;
-use crate::moderation::ModerationOptions;
 
 async fn load_version(
     transaction: &mut ::sqlx::Transaction<'_, ::sqlx::Postgres>,
