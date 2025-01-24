8d70d0ef1b597715fbccdf03169db37ed88d4755
Aidan <aidan@futo.org>
2025-01-23 22:43:30 -0800

    Update Cache-Control headers to use long s-maxage and short max-age

--- server/src/handlers/get_events.rs
+++ server/src/handlers/get_events.rs
@@ -61,7 +61,7 @@ async fn handler_inner(
             ::warp::http::StatusCode::OK,
         ),
         "Cache-Control",
-        "public, max-age=30",
+        "public, s-maxage=30, max-age=5",
     )))
 }
 
