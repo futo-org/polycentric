c877d05fff10b2b145ccef4528aacdd6e87ca06a
Trevor <trevor@futo.org>
2025-07-16 10:11:08 -0500

    added pull to refresh

--- packages/polycentric-react/src/lib/pages/templates/FeedPage/index.tsx
+++ packages/polycentric-react/src/lib/pages/templates/FeedPage/index.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from 'react';
+import PullToRefresh from 'react-pull-to-refresh';
 import { InfiniteScrollWithRightCol } from '../../../components/layout/infinitescrollwithrightcol';
 import { FeedHookAdvanceFn, FeedHookData } from '../../../hooks/feedHooks';
+import { useIsMobile } from '../../../hooks/styleHooks';
 
 export const FeedPage = ({
   data,
@@ -11,11 +13,25 @@ export const FeedPage = ({
   advanceFeed: FeedHookAdvanceFn;
   rightCol: ReactElement;
 }) => {
-  return (
+  const isMobile = useIsMobile();
+
+  const handleRefresh = async () => {
+    await Promise.resolve(advanceFeed());
+  };
+
+  const content = (
     <InfiniteScrollWithRightCol
       data={data}
       advanceFeed={advanceFeed}
       rightCol={rightCol}
     />
+  );
+
+  return isMobile ? (
+    <PullToRefresh onRefresh={handleRefresh}>
+      {content}
+    </PullToRefresh>
+  ) : (
+    content
   );
 };
