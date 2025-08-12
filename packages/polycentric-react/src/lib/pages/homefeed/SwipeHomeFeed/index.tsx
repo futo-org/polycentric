ef42112e0ad5682c7da3222f89d0f1ef00df6628
Trevor <trevor@futo.org>
2025-08-12 11:45:38 -0500

    Remove search functionality from SwipeHomeFeed header - simplify to just show topic names
    - Removed search input field and related components
    - Simplified TopicSwipeSelect to only show Explore/Following topic names
    - Cleaned up unused imports and components
    - Maintained dark mode styling and navigation functionality

--- packages/polycentric-react/src/lib/pages/homefeed/SwipeHomeFeed/index.tsx
+++ packages/polycentric-react/src/lib/pages/homefeed/SwipeHomeFeed/index.tsx
@@ -1,36 +1,20 @@
 import { IonContent, IonHeader, IonMenuToggle, isPlatform } from '@ionic/react';
 import {
-  createContext,
   useCallback,
   useContext,
   useEffect,
   useMemo,
-  useState,
+  useState
 } from 'react';
 import { Swiper as SwyperType } from 'swiper';
 import 'swiper/css';
 import './style.css';
 
-import { Transition } from '@headlessui/react';
 import { PencilSquareIcon } from '@heroicons/react/24/outline';
-import { Models, Util } from '@polycentric/polycentric-core';
 import { Controller } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { MobileSwipeTopicContext } from '../../../app/contexts';
-import { Feed, PopupComposeFullscreen } from '../../../components';
-import { Link } from '../../../components/util/link';
-import { useSearchPostsFeed } from '../../../hooks/feedHooks';
-import { useProcessHandleManager } from '../../../hooks/processHandleManagerHooks';
-import {
-  useQueryCRDTSet,
-  useQueryTopStringReferences,
-} from '../../../hooks/queryHooks';
-import {
-  normalizeTopic as normalizeTopicString,
-  useTopicDisplayText,
-  useTopicLink,
-} from '../../../hooks/utilHooks';
-import { numberTo4Chars } from '../../../util/etc';
+import { PopupComposeFullscreen } from '../../../components';
 import { ExploreFeed } from './ExploreFeed';
 import { FollowingFeed } from './FollowingFeed';
 import { TopicFeed } from './TopicFeed';
@@ -52,129 +36,7 @@ const MenuIcon = () => (
   </svg>
 );
 
-const PopupSearchMenuContext = createContext<{
-  close: () => void;
-}>({
-  close: () => {},
-});
 
-const PostSearchResults = ({ query }: { query: string }) => {
-  const [data, advanceFeed, nothingFound] = useSearchPostsFeed(query);
-
-  return (
-    <Feed data={data} advanceFeed={advanceFeed} nothingFound={nothingFound} />
-  );
-};
-
-const searchTypeNames = ['topics', 'posts'];
-
-const TopicSearchResultsItem = ({
-  topic,
-}: {
-  topic: Models.AggregationBucket.Type;
-}) => {
-  const topicLink = useTopicLink(topic.key);
-  const displayText = useTopicDisplayText(topic.key);
-  const { close } = useContext(PopupSearchMenuContext);
-  return (
-    <Link
-      className="flex space-x-3 items-center w-full p-1 rounded-[1.75rem] hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors duration-200 ease-in-out cursor-pointer"
-      key={topic.key}
-      routerLink={topicLink}
-      routerDirection="forward"
-      onClick={close}
-    >
-      <div className="bg-gray-100 dark:bg-gray-700 h-12 w-12 text-sm rounded-full flex justify-center items-center text-black dark:text-white">
-        {numberTo4Chars(topic.value)}
-      </div>
-      <div className="text-lg text-black dark:text-white">{displayText}</div>
-    </Link>
-  );
-};
-
-const TopicSearchResults = ({ query }: { query?: string }) => {
-  const hookOptions = useMemo(() => {
-    return { query, minQueryChars: 3 };
-  }, [query]);
-
-  const topTopicsAll = useQueryTopStringReferences(hookOptions);
-
-  // load blocked topics
-  const { processHandle } = useProcessHandleManager();
-  const system = useMemo(() => processHandle.system(), [processHandle]);
-
-  const [blockedEvents, advanceBlocked] = useQueryCRDTSet(
-    system,
-    Models.ContentType.ContentTypeBlockTopic,
-    100,
-  );
-
-  useEffect(() => {
-    advanceBlocked();
-  }, [advanceBlocked]);
-
-  const blockedSet = useMemo(() => {
-    const s = new Set<string>();
-    blockedEvents.forEach((e) => {
-      const v = e.lwwElementSet?.value;
-      if (v) {
-        const plain = Util.decodeText(v);
-        s.add(normalizeTopicString(plain));
-      }
-    });
-    return s;
-  }, [blockedEvents]);
-
-  const topTopics = useMemo(() => {
-    return topTopicsAll.filter(
-      (t) => !blockedSet.has(normalizeTopicString(t.key)),
-    );
-  }, [topTopicsAll, blockedSet]);
-
-  return (
-    <div className="flex flex-col space-y-2 w-[18rem] pt-4">
-      {topTopics.map((topic) => (
-        <TopicSearchResultsItem topic={topic} key={topic.key} />
-      ))}
-    </div>
-  );
-};
-
-const SearchArea = ({
-  realTimeQuery,
-  enterPressedQuery,
-}: {
-  realTimeQuery: string;
-  enterPressedQuery: string;
-}) => {
-  const [searchType, setSearchType] = useState<'topics' | 'posts'>('topics');
-
-  return (
-    <div className="flex flex-col items-center h-full">
-      <div className="flex w-64 justify-around">
-        {searchTypeNames.map((searchTypeName) => (
-          <button
-            key={searchTypeName}
-            className={`font-medium text-md p-2 rounded-full text-black dark:text-white ${
-              searchTypeName === searchType
-                ? 'bg-gray-200 dark:bg-gray-600'
-                : ''
-            }`}
-            onClick={() => setSearchType(searchTypeName as 'topics' | 'posts')}
-          >
-            {searchTypeName}
-          </button>
-        ))}
-      </div>
-      {/* post and account searches are much more expensive and don't have an expectation of autocomplete */}
-      {searchType === 'topics' ? (
-        <TopicSearchResults query={realTimeQuery} />
-      ) : searchType === 'posts' ? (
-        <PostSearchResults query={enterPressedQuery} />
-      ) : undefined}
-    </div>
-  );
-};
 
 const TopicSwipeSelect = ({
   topics,
@@ -187,114 +49,35 @@ const TopicSwipeSelect = ({
   setHeaderSwiper: (swiper: SwyperType) => void;
   handleSlideChange: (swiper: SwyperType) => void;
 }) => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [enterPressedQuery, setEnterPressedQuery] = useState('');
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [headerSwiper, setHeaderSwiperState] = useState<SwyperType | undefined>(
     undefined,
   );
-
-  const { close } = useContext(PopupSearchMenuContext);
 
   const handleHeaderSwiper = (swiper: SwyperType) => {
     setHeaderSwiperState(swiper);
     setHeaderSwiper(swiper);
   };
 
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === 'Enter') {
-      setEnterPressedQuery(searchQuery);
-    }
-  };
-
-  const handleSearchClick = () => {
-    setIsSearchOpen(true);
-  };
-
-  const handleClose = () => {
-    setIsSearchOpen(false);
-    setSearchQuery('');
-    setEnterPressedQuery('');
-    close();
-  };
-
   return (
     <div className="flex-1 flex justify-center">
-      <div className="relative">
-        <div className="border border-gray-200 dark:border-gray-600 rounded-full h-12 w-64 flex items-center">
-          <input
-            type="text"
-            placeholder="Search topics..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onKeyDown={handleKeyDown}
-            onClick={handleSearchClick}
-            className="w-full h-full outline-none pl-6 text-2xl bg-transparent text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400"
-          />
-        </div>
-        <Swiper
-          className="absolute top-0 left-0 w-64 h-12"
-          modules={[Controller]}
-          onSwiper={handleHeaderSwiper}
-          controller={{ control: feedSwiper }}
-          edgeSwipeDetection={true}
-          edgeSwipeThreshold={50}
-          onSlideChange={handleSlideChange}
-          cssMode={false}
-        >
-          {topics.map((topic) => (
-            <SwiperSlide key={topic}>
-              <div className="w-full h-full flex items-center justify-center">
-                <h1 className="text-2xl text-black dark:text-white">{topic}</h1>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-
-      <Transition
-        show={isSearchOpen}
-        enter="transition-all duration-300 ease-out"
-        enterFrom="h-12 w-64 rounded-[1.5rem] border-gray-200 dark:border-gray-600"
-        enterTo="h-[100dvh] w-full rounded-0 border-gray-200 dark:border-gray-600"
-        leave="transition-all duration-300 ease-in"
-        leaveFrom="h-[100dvh] w-full rounded-0 border-gray-200 dark:border-gray-600"
-        leaveTo="h-12 w-64 rounded-0 border-gray-200 dark:border-gray-600 rounded-[1.5rem]"
-        className={
-          'fixed z-20 bg-white dark:bg-gray-900 overflow-clip ease-in-out'
-        }
-        style={{
-          top: '4.5rem',
-          left: '50%',
-          transform: 'translateX(-50%)',
-        }}
+      <Swiper
+        className="h-12 w-64"
+        modules={[Controller]}
+        onSwiper={handleHeaderSwiper}
+        controller={{ control: feedSwiper }}
+        edgeSwipeDetection={true}
+        edgeSwipeThreshold={50}
+        onSlideChange={handleSlideChange}
+        cssMode={false}
       >
-        <div className="relative w-full h-full">
-          <button
-            onClick={handleClose}
-            className="w-12 h-12 border border-gray-300 dark:border-gray-600 rounded-full relative bg-white dark:bg-gray-800 pointer-events-auto"
-            style={{
-              position: 'absolute',
-              top: '1rem',
-              right: '1rem',
-              zIndex: 30,
-            }}
-          >
+        {topics.map((topic) => (
+          <SwiperSlide key={topic}>
             <div className="w-full h-full flex items-center justify-center">
-              <div className="w-6 h-0.5 bg-gray-400 dark:bg-gray-300 transform rotate-45 absolute"></div>
-              <div className="w-6 h-0.5 bg-gray-400 dark:bg-gray-300 transform -rotate-45 absolute"></div>
+              <h1 className="text-2xl text-black dark:text-white">{topic}</h1>
             </div>
-          </button>
-          <div className="fixed w-full top-[4.5rem] h-[100dvh] bg-white dark:bg-gray-900 overflow-y-auto pointer-events-auto">
-            <div className="flex justify-center pt-8">
-              <SearchArea
-                realTimeQuery={searchQuery}
-                enterPressedQuery={enterPressedQuery}
-              />
-            </div>
-          </div>
-        </div>
-      </Transition>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
