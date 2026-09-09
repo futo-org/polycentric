434bcc7abf7c8dd94eadda7fbb9701e41bed39d2
Thomas de Zeeuw <thomas@futo.org>
2026-09-09 16:18:48 +0200

    Enable search_posts_omit_labels test

--- services/server/tests/src/search.rs
+++ services/server/tests/src/search.rs
@@ -580,7 +580,6 @@ async fn search_posts_order_by_latest() {
 }
 
 #[tokio::test]
-#[ignore] // Currently failing, to be fixed in #201.
 async fn search_posts_omit_labels() {
     let query = random_string();
     let label = random_string();
