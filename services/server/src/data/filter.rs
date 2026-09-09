8d1b27643f19d996f5f99fc656b8146f0cfc8160
Thomas de Zeeuw <thomas@futo.org>
2026-09-09 11:50:08 +0200

    Add filter::deleted_or_blocked

--- services/server/src/data/filter.rs
+++ services/server/src/data/filter.rs
@@ -25,6 +25,29 @@ where
     Ok(Filtered { rows, page_info })
 }
 
+/// Filter out deleted events or events by blocked identities..
+pub async fn deleted_or_blocked<Ctx, Params, Row, SortedBy>(
+    _: &Ctx,
+    _: &Params,
+    fetched: Fetched<Row, SortedBy>,
+    hydration: &HydrationState,
+) -> Result<Filtered<Row, SortedBy>, Status>
+where
+    Row: EventRow,
+{
+    let Fetched { rows, page_info } = fetched;
+    let rows = rows
+        .into_iter()
+        .filter(|row| {
+            !hydration
+                .blocked_identities
+                .contains(&row.as_event().identity)
+                && !hydration.deletes_by_target.contains_key(&row.event_key())
+        })
+        .collect();
+    Ok(Filtered { rows, page_info })
+}
+
 pub struct Filtered<Row, SortedBy> {
     pub rows: Vec<Row>,
     pub page_info: PageInfo<SortedBy>,
